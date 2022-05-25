08344ef91c2d83281f1ee6518eb54e106a925051
krzysiek <you@example.com>
2022-05-25 13:27:46 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todoapp-front-v2';
+  show:boolean = true;
+
+  ngOnInit(): void {
+    this.show = true;
+  }
+
+  hideApp() {
+    this.show = false;
+  }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from "@angular/common/http";
-import { Task } from '../Task';
+import { Task } from '../task';
 @Injectable({
   providedIn: 'root'
 })
