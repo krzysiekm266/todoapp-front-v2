f9aac346a9938206943f7385b82e43c2989cc815
krzysiek <you@example.com>
2022-07-02 13:47:59 +0200

    readme changes,add welcome component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 const routes: Routes = [
+  { path:'',redirectTo:'welcome',pathMatch:'full'},
+  { path:'welcome',component:WelcomeComponent },
   { path: 'tasks', loadChildren: () => import('./modules/tasks/tasks.module').then(m => m.TasksModule) },
   { path: 'about', loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule) },
 
